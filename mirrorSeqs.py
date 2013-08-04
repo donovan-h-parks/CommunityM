bdd7b637e2ab11f7b1b1edc91cac9fbbec9c56af
dparks1134 <donovan.parks@gmail.com>
2013-08-05 09:34:41 +1000

    Added GPL notice to top of all files.

--- mirrorSeqs.py
+++ mirrorSeqs.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+
 """
 Produce FASTQ file that mirrors reads in a single FASTA file.
 """
@@ -13,7 +31,7 @@ __maintainer__ = 'Donovan Parks'
 __email__ = 'donovan.parks@gmail.com'
 __status__ = 'Development'
 
-import os, sys, argparse
+import sys, argparse
 
 class MirrorSeqs(object):
   def __init__(self):
